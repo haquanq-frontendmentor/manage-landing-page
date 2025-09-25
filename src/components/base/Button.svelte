ac3922f81d76b82ddd1a01068cb2968e5690252e
Ha Quanq <quangha0120010@gmail.com>
2025-09-25 23:33:19 +0700

    Add base components: Button, Paragraph

---
+++ src/components/base/Button.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import type { Snippet } from "svelte";
+  import { cn } from "../../utils/cn";
+  import type { HTMLButtonAttributes } from "svelte/elements";
+
+  type ButtonProps = {
+    xClass?: string;
+    children?: Snippet;
+    link?: string;
+    type?: HTMLButtonAttributes["type"];
+  };
+
+  const { link, children, xClass, type = "button", ...restProps }: ButtonProps = $props();
+  const className =
+    "rounded-full bg-orange-400 cursor-pointer text-100 leading-4 py-[0.875rem] pl-8 pr-[2.0625rem] font-medium text-gray-50 block w-fit";
+</script>
+
+{#if link !== undefined}
+  <a class={cn(className, xClass)} href={link} {...restProps}>{@render children?.()}</a>
+{:else}
+  <button class={cn(className, xClass)} type="button" {...restProps}>{@render children?.()}</button>
+{/if}
---
+++ src/components/base/Paragraph.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import type { Snippet } from "svelte";
+  import { cn } from "../../utils/cn";
+
+  type ParagraphProps = {
+    xClass?: string;
+    children: Snippet;
+  };
+  const { xClass, children }: ParagraphProps = $props();
+</script>
+
+<p class={cn("text-base leading-[1.625rem]", xClass)}>{@render children?.()}</p>
