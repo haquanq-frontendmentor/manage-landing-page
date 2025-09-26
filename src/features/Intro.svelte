0788cbbd16a2ddad39f3c1e649c3f758baf094f6
Ha Quanq <quangha0120010@gmail.com>
2025-09-26 08:28:31 +0700

    Remove intro image outline

--- src/features/Intro.svelte
+++ src/features/Intro.svelte
@@ -8,7 +8,7 @@
   class="grid gap-x-[clamp(4rem,-5.1875rem+19.1406vw,7.0625rem)] gap-y-8 pt-12 md:grid-flow-dense md:grid-cols-[1fr_min(35rem,50%)] md:pt-[6.875rem]"
 >
   <div class="flex justify-center md:col-start-2 md:justify-start">
-    <img class="outline-1 -outline-offset-1 md:max-w-[500%]" src={IntroIllustration} alt="" />
+    <img class="md:max-w-[500%]" src={IntroIllustration} alt="" />
   </div>
   <div class="flex flex-col items-center text-center md:items-start md:pt-[3.6875rem] md:text-start">
     <h1 class="text-600 md:text-700 mb-3 max-w-120 leading-10 font-bold tracking-tight md:mb-[2.125rem] md:leading-16">
